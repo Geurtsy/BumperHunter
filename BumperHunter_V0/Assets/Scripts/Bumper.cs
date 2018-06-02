809f2928e5c8c83f0d17638ab5de09d53b1730f9
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-06-03 07:27:16 +0800

    Bumper functionality added

---
+++ BumperHunter_V0/Assets/Scripts/Bumper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+[RequireComponent(typeof(Collider))]
+public class Bumper : MonoBehaviour
+{
+
+    public float _length;
+    [SerializeField] private bool _preset;
+    private Light _light;
+    private Renderer _haloRenderer;
+
+    [SerializeField] private Color _minBumperColour;
+    [SerializeField] private Color _maxBumperColour;
+
+    private void Start()
+    {
+        _light = GetComponent<Light>();
+        _haloRenderer = transform.GetChild(0).GetComponent<Renderer>();
+
+        if(!_preset)
+        {
+            _length = Random.Range(1.0f, 100.0f);
+        }
+
+        UpdateColours();
+    }
+
+    private void UpdateColours()
+    {
+        Color newColor = Color.Lerp(_minBumperColour, _maxBumperColour, _length / 100);
+        _light.color = newColor;
+        _haloRenderer.material.color = newColor + new Color(0, 0, 0, 0.3f);
+    }
+}
--- BumperHunter_V0/Assets/Scripts/Controllers/Player/CtrlPlayer.cs
+++ BumperHunter_V0/Assets/Scripts/Controllers/Player/CtrlPlayer.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(AudioSource))]
 public class CtrlPlayer : MonoBehaviour {
 
+    public float Rage
+    {
+        get { return Rage; }
+        set
+        {
+            if(value + Rage > 100.0f)
+            {
+                RageOut();
+            }
+        }
+    }
+
+    [HideInInspector] public float _stamina = 100.0f;
     [SerializeField] private VirtualJoystick _joystick;
     private IMove _movingMech;
+
+    [SerializeField] private AudioSource _myAudioSource;
+    [SerializeField] private AudioClip[] _rageSounds;
+    [SerializeField] private AudioClip[] _bumperCollectSounds;
 
     private void Start()
     {
@@ -27,5 +44,22 @@ public class CtrlPlayer : MonoBehaviour {
             Quaternion camRot = Camera.main.transform.rotation;
             _joystick.Rotate(camRot);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var go = other.gameObject;
+
+        if(go.tag == "Bumper")
+        {
+            Bumper bumper = go.GetComponent<Bumper>();
+            Rage -= bumper._length;
+            _myAudioSource.PlayOneShot(_bumperCollectSounds[Random.Range(0, _bumperCollectSounds.Length)]);
+        }
+    }
+
+    private void RageOut()
+    {
+        _myAudioSource.PlayOneShot(_rageSounds[Random.Range(0, _bumperCollectSounds.Length)]);
     }
 }
